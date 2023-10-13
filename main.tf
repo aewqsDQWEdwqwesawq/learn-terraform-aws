c6764c03f5b5adf53cc6792ac41472491f065608
gb23-seo <seogb8@gmail.com>
2023-10-13 11:22:49 +0900

    6

--- main.tf
+++ main.tf
@@ -12,6 +12,17 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+resource "aws_key_pair" "pka" {
+  key_name   = "pka"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxF6Do/MzS6LFg89W+k3dVcN6eQI/UAfO4bqsLfAxr3jHWA5oHojRtjfiZLqSenF1OISWda/l6Ugb5fO8oTkSzJRIZ3hGXxYDHnXuMSGpS3ZTQqgEjVxyevbDLtqJshaAdaoHvLjtAXCTln4o1vCDWS0iAvaYLoYDjZa9mSBI3+9BsGJbU9M3F38kmx0ajPOqUf3PBRA0m2+dHG4yGEVFEqdwhtZtHkmeEMTOaUAj8VT5HkHfkaI8rVibkwTq2EechyKKwL7l1nbMrEtXghp5G17dbDYrbYN7aE7zVTWl66ds5WXOF8Tc8sS4bkMWrSzbbs3w26TrWpEGX2ejOnZIF rsa-key-20231013"
+
+  tags = {
+    "Name" = "pka"
+  }
+}
+
+
+
 # Bastion Host instance
 resource "aws_instance" "Bastion" {
   ami = data.aws_ami.ubuntu.id
@@ -19,6 +30,7 @@ resource "aws_instance" "Bastion" {
   associate_public_ip_address = true
   security_groups = [aws_security_group.public_sub.id]
   subnet_id = aws_subnet.public_subnets.id
+  key_name = aws_key_pair.pka.key_name
   
   tags = {
     "Name" = "BastionHost"
