bfecbd18fd7dd64a73d7bdbcf091494150899348
gb23-seo <seogb8@gmail.com>
2023-10-13 15:24:20 +0900

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -16,7 +16,13 @@ resource "aws_security_group" "app_server" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.2.0/23"]
   }
-
+  
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 8086
