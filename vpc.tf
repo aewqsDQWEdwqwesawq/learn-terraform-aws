436eefb472cadf3011e7e204517aefc7d0f7a466
gb23-seo <seogb8@gmail.com>
2023-10-13 15:04:47 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -36,8 +36,6 @@ resource "aws_subnet" "public_subnets" {
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-northeast-2a"
 
-  depends_on = [aws_internet_gateway.testig]
-
   tags = {
     "Name" = "PublicSubnet"
   }
@@ -56,28 +54,29 @@ resource "aws_internet_gateway" "testig" {
 # EIP for NATGW
 resource "aws_eip" "natip" {
   domain = "vpc"
-  
 }
 
-# EIP for bastion
-resource "aws_eip" "bastionip" {
-  instance = aws_instance.Bastion.id
-  domain   = "vpc"
+resource "aws_eip" "natip2" {
+  domain = "vpc"
 }
 
 # NAT GW
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.natip.id
-  subnet_id     = aws_subnet.public_subnets.id
+  subnet_id     = aws_subnet.private_subnets.id
 }
 
+resource "aws_nat_gateway" "natgw2" {
+  allocation_id = aws_eip.natip2.id
+  subnet_id = aws_subnet.private_subnets2.id
+}
 
 # route table
 resource "aws_route_table" "publicrt" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = ["aws_subnet.public_subnets"]
+    cidr_block = "10.0.1.0/24"
     gateway_id = aws_internet_gateway.testig.id
   }
   
@@ -86,11 +85,21 @@ resource "aws_route_table" "privatert" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block     = ["aws_subnet.private_subnets.id","aws_subnet.private_subnets2.id"]
+    cidr_block     = "10.0.2.0/24"
     nat_gateway_id = aws_nat_gateway.natgw.id
   }
 }
 
+resource "aws_route_table" "privatert2" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "10.0.3.0/24"
+    nat_gateway_id = aws_nat_gateway.natgw2.id
+  }
+}
+
+# route table association
 resource "aws_route_table_association" "publicrt" {
   subnet_id = aws_subnet.public_subnets.id
   route_table_id = aws_route_table.publicrt.id
@@ -103,5 +112,5 @@ resource "aws_route_table_association" "privatert1" {
 
 resource "aws_route_table_association" "privatert2" {
   subnet_id = aws_subnet.private_subnets2.id
-  route_table_id = aws_route_table.privatert.id
+  route_table_id = aws_route_table.privatert2.id
 }
