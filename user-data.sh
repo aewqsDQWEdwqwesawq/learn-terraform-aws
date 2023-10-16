7fd13f78ffe7ea92bc775e7547169fa716852c0a
gb23-seo <seogb8@gmail.com>
2023-10-16 11:41:36 +0900

    14141

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install nginx
+sudo apt update
+sudo apt install nginx -y
 sudo systemctl restart nginx
 
 
@@ -9,5 +9,5 @@ wget -q https://repos.influxdata.com/influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
 echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 
-sudo apt-get update && sudo apt-get install telegraf
+sudo apt update && sudo apt install -y telegraf
 sudo systemctl restart telegraf
