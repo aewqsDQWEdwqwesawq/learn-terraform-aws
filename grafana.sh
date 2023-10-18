1625927bd253dd09bdd4f01469537b0e215dd68b
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-18 13:01:35 +0900

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y apt-transport-https software-properties-common wget
-sudo mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-# Updates the list of available packages
-sudo apt-get update
-# Installs the latest OSS release:
-sudo apt-get install grafana
+sudo apt install adduser libfontconfig1 musl -y
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_10.1.5_amd64.deb
+sudo dpkg -i grafana-enterprise_10.1.5_amd64.deb
 
-sudo systemctl start grafana
+sudo systemctl daemon-reload
+sudo systemctl enable grafana-server
+sudo systemctl restart grafana-server
