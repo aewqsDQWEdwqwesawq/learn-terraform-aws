e83d6a4b517379a44f7d88713279c8b8837a516e
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-18 13:04:43 +0900

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -81,3 +81,32 @@ resource "aws_security_group" "public_sub" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# for grafana
+
+resource "aws_security_group" "grafana_sub" {
+  name  = "SGforgrafana"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
