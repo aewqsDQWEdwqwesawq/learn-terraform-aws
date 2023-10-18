1e341eb817b8576c67a5b11645f30405fc6b681c
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-18 13:02:51 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,14 @@ resource "aws_instance" "Bastion" {
   }
 }
 
+# grafana server
+
+resource "aws_instance" "grafana" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.app_server.id]
+  subnet_id = aws_subnet.private_subnets.id
+  tags = {
+    "Name" = "Grafana"
+  }
+}
