1e9e03d97112d869becf323ed543cdd18d45aa06
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-18 13:16:54 +0900

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -30,8 +30,8 @@ resource "aws_lb_target_group" "testlb-target" {
     protocol = "HTTP"
     healthy_threshold = 2
     unhealthy_threshold = 2
-    interval = 30
-    timeout = 5
+    interval = 60
+    timeout = 30
   }
 }
 
