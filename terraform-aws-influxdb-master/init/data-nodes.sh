fb3ef4f3867c69b8669f4c2149808804b8de951f
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-18 12:50:13 +0900

    Update data-nodes.sh

--- terraform-aws-influxdb-master/init/data-nodes.sh
+++ terraform-aws-influxdb-master/init/data-nodes.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+/*
 wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.7.1-arm64.deb
-sudo dpkg -i influxdb2-2.7.1-arm64.deb
-
+sudo dpkg -i influxdb2-2.7.1-arm64.deb/*
+sudo apt update
+sudo apt install influxdb -y
 sudo systemctl start influxdb
+sudo apt install influxdb-client -y
+sudo systemctl restart influxdb-client
+
+sudo systemctl enable influxdb
+sudo systmectl enable influxdb-client
