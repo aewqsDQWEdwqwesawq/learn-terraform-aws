84296111500271ed8ebd613e3ecc7d01410fcd5f
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-19 17:44:36 +0900

    Update data-nodes.sh

--- data-nodes.sh
+++ data-nodes.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-/*
-wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.7.1-arm64.deb
-sudo dpkg -i influxdb2-2.7.1-arm64.deb
-*/
+
+#wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.7.1-arm64.deb
+#sudo dpkg -i influxdb2-2.7.1-arm64.deb
+
 sudo apt update
 sudo apt install influxdb -y
 sudo systemctl start influxdb
