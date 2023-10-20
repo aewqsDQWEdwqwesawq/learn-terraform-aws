20541d264e055487b2a7d19034ef460b97d33f26
서기범 <>
2023-10-20 15:33:39 +0900

    Update keycloak.sh

--- keycloak.sh
+++ keycloak.sh
@@ -2,8 +2,8 @@
 
 sudo apt update
 sudo apt install openjdk-11-jdk
-wget https://github.com/keycloak/keycloak/releases/download/17.0.0/keycloak-17.0.0.tar.gz
-tar -zxvf keycloak-17.0.0.tar.gz
+wget https://github.com/keycloak/keycloak/releases/download/21.1.2/keycloak-21.1.2.tar.gz
+tar -zxvf keycloak-22.0.4.tar.gz
 cd keycloak-17.0.0/
 export KEYCLOAK_ADMIN=admin
 export KEYCLOAK_ADMIN_PASSWORD=password
