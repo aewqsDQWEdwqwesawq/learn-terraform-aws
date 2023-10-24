0e151336a6ac94f16e5d5741c95f121bbe4c75b5
서기범 <>
2023-10-24 14:11:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "aws_instance" "grafana" {
 }
 
 # DB server
-resource "aws_instance" "InfluxDB" {
+resource "aws_instance" "InfluxDB1" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.db.id]
