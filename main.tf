9ad9259f62dd63449e6a88e6fc61b63ffdfbd359
qwdpok <130974853+canyonhill@users.noreply.github.com>
2023-10-26 15:49:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,16 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+# latest ubuntu ami
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-*"]
+  }
+}
+
 # key
 resource "tls_private_key" "pk" {
   algorithm = "RSA"
